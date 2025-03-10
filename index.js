718189cc1dbcc5723dae69fdd4c51b6baa83f072
Ana Rogalo <rogaloana@gmail.com>
2025-03-10 23:48:25 +0100

    commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import express from 'express'
+import cors from 'cors';
+import { connectToDatabase } from './db.js';
 
 const app = express();
 const PORT = 3000;
+app.use(express.json());
+app.use(cors());
+
+const db = await connectToDatabase();
+
+
+app.listen(PORT, error => {
+  if (error) {
+    console.log('Greška prilikom pokretanja servera', error);
+  }
+  console.log(`Pizza poslužitelj radi na http://localhost:${PORT}`);
+});
